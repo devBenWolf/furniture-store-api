71e3b038656a82336e8a10ab3f3982d1f7ed306e
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-08-11 16:43:33 -0400

    added basic express boilerplate to app.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('04 Store API')
+require(`dotenv`).config()
+const express = require(`express`)
+const app = express()
+const notFoundMiddleware = require(`./middleware/not-found.js`)
+const errorMiddleware = require(`./middleware/error-handler.js`)
+
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get(`/`, (req, res) => {
+    res.send(`<h1>Store API</h1><a href = "/api/v1/products">Link to products</a>`)
+})
+
+// products route
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async() => {
+    try {
+        // connectDB
+        app.listen(port)
+        console.log(`Server listening on port ${port}...`)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
